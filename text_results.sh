e620c3671ff970bcbc29cb972711563ca54b90bd
rlica <licarazvan90@res-net-gpu.c.certain-nexus-204507.internal>
2018-06-09 15:03:54 +0000

    Added some scripts

--- text_results.sh
+++ text_results.sh
@@ -1,1 +1,1 @@
-python generate_result_video/generate_result_text.py ./output.json ./videos/ ./results ./class_names_list 5
+python generate_result_video/generate_result_text.py ./output.json ../videos/ ./results ./class_names_list 5
