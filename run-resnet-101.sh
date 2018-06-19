54329a9d194449e0b80082de1ee26c28a2f233b0
rlica <licarazvan90@res-net-gpu.c.certain-nexus-204507.internal>
2018-06-19 07:31:54 +0000

    Tested with resnet-101

---
+++ run-resnet-101.sh
@@ -0,0 +1,1 @@
+python main.py --input ./input --video_root ../videos --output ./output.json --model ../models/resnet-101-kinetics.pth --model_depth 101 --resnet_shortcut B --mode score
--- run.sh
+++
@@ -1,1 +0,0 @@
-python main.py --input ./input --video_root ../videos --output ./output.json --model ../models/resnet-34-kinetics.pth --mode score
