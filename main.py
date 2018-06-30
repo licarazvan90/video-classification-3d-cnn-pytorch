7717f6390e4131079efc2acac658a74622fbf929
Razvan Lica <razvan.lica@cern.ch>
2018-06-30 19:05:49 +0200

    jpg

--- main.py
+++ main.py
@@ -52,17 +52,17 @@ if __name__=="__main__":
             print(video_path)
             subprocess.call('mkdir tmp', shell=True)
             subprocess.call('ffmpeg -i {} tmp/image_%05d.jpg'.format(video_path),
-                            shell=True)
+                           shell=True)
 
             result = classify_video('tmp', input_file, class_names, model, opt)
             outputs.append(result)
 
-            subprocess.call('rm -rf tmp', shell=True)
+#            subprocess.call('rm -rf tmp', shell=True)
         else:
             print('{} does not exist'.format(input_file))
 
     if os.path.exists('tmp'):
-        subprocess.call('rm -rf tmp', shell=True)
+#        subprocess.call('rm -rf tmp', shell=True)
 
     with open(opt.output, 'w') as f:
         json.dump(outputs, f)
