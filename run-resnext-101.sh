3132399233dfad661926d7e4535efb15043fd48f
Razvan Lica <razvan.lica@cern.ch>
2018-06-30 11:45:49 +0200

    testing resnext-101

---
+++ run-resnext-101.sh
@@ -0,0 +1,1 @@
+python main.py --input ./input --video_root ../videos --output ./output.json --model ../models/resnext-101-kinetics.pth --model_depth 101 --resnet_shortcut B --resnext_cardinality 32 --mode score 
