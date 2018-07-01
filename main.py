d343fccdd64d4a292e958ebbe3eae0cb12a6f0c3
rlica <licarazvan90@res-net-gpu.c.certain-nexus-204507.internal>
2018-07-01 08:46:58 +0000

    testing other datasets

--- main.py
+++ main.py
@@ -42,27 +42,27 @@ if __name__=="__main__":
     if opt.verbose:
         ffmpeg_loglevel = 'info'
 
-#    if os.path.exists('tmp'):
-#        subprocess.call('rm -rf tmp', shell=True)
+    if os.path.exists('tmp'):
+        subprocess.call('rm -rf tmp', shell=True)
 
     outputs = []
     for input_file in input_files:
         video_path = os.path.join(opt.video_root, input_file)
         if os.path.exists(video_path):
             print(video_path)
-#            subprocess.call('mkdir tmp', shell=True)
-#            subprocess.call('ffmpeg -i {} tmp/image_%05d.jpg'.format(video_path),
-#                           shell=True)
+            subprocess.call('mkdir tmp', shell=True)
+            subprocess.call('ffmpeg -i {} tmp/image_%05d.jpg'.format(video_path),
+                            shell=True)
 
             result = classify_video('tmp', input_file, class_names, model, opt)
             outputs.append(result)
 
-#            subprocess.call('rm -rf tmp', shell=True)
+            subprocess.call('rm -rf tmp', shell=True)
         else:
             print('{} does not exist'.format(input_file))
 
-#   if os.path.exists('tmp'):
-#        subprocess.call('rm -rf tmp', shell=True)
+    if os.path.exists('tmp'):
+        subprocess.call('rm -rf tmp', shell=True)
 
     with open(opt.output, 'w') as f:
         json.dump(outputs, f)
